23a751174d65151aba8d9e7fd3b19d9f52606e32
fiskar <artem@hismatullin.ru>
2019-12-25 00:56:17 +0500

    Unique classes for widgets added

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function countI() {
-	for (let i = 1; i < 11;i++) {
-	return i;
-}
-}
-function addWidget() {
-	i = countI();
+function Generator() {};
+Generator.prototype.rand = 1;
+Generator.prototype.getId = function() {
+	return this.rand++;
+};
+let idGen = new Generator();
 
+function addWidget() {
+	i = (idGen.getId());
+	
 	let widget = document.createElement('div');
 
 	widget.className = `widget-${i}`;
-	widget.insertAdjacentHTML('beforeend', `<input type="button" value="del" onclick="removeWidget();">`);	
+	widget.insertAdjacentHTML('beforeend', `<input type="button" title="Удалить виджет" value="del" onclick="removeWidget();">`);	
+
 	dashboard.append(widget);
-}
+	};
 
 function removeWidget() {
 	document.getElementsByClassName(`widget-${i}`);
